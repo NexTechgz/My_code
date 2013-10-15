aee0a9c42841085c04bd349cbbd5513a0de81f3e
NexTechgz <nextechgz@gmail.com>
2013-10-15 11:02:23 +0300

    THE EDITED CODE FOR f.php
    this si just for learning

--- f.php
+++ f.php
@@ -1,4 +1,5 @@
 <?php
+// WYCLIFF HAS EDITED THIS CODE 
 $p= mysql_connect("localhost","root","");
 $pp=mysql_select_db("cert");
 $pop="select * from loc";
@@ -9,11 +10,6 @@ while($u=mysql_fetch_array($ppop))
 echo "<tr><td>{$u[0]}</td></tr>";
 }
 echo "</table>";
-echo "<table align=\"center\">";
-echo "<tr><td>Loop<td></tr>";
-echo "<tr><td>Moop<td></tr>";
-echo "<tr><td>Noop<td></tr>";
-echo "<tr><td>WOOP<td></tr>";
-echo "<tr><td>VWET<td></tr>";
+
 echo "</table>";
 ?>
